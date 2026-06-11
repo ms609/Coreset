1dff3286640cb511a83c69b2e9763bd25000718a
R script <martin.smith@durham.ac.uk>
2026-06-11 14:25:48 +0100

    Clarify not k-centre objective

--- R/score.R
+++ R/score.R
@@ -1,10 +1,9 @@
 # score.R
 
-#' Minimum pairwise distance within a selection (T_k = k-centre objective)
+#' Minimum pairwise distance within a selection
 #'
-#' Returns the minimum pairwise distance among selected points, the canonical
-#' k-centre objective \eqn{T_k}. Higher values indicate a more spread-out
-#' (better) selection.
+#' Returns the minimum pairwise distance among selected points \eqn{T_k}.
+#' A set of points that is more dispersed will exhibit a higher value.
 #'
 #' @param d Pairwise distance matrix or `dist` object. Ignored when `points`
 #'   is supplied.
